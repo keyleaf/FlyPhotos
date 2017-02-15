2e10c573b35fec9d257021b7659fe214677e4cb4
zhou fei <fzhou@cccis.com>
2017-02-15 17:06:20 +0800

    创建node服务，完成初始化启动

---
+++ server/env.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // mongo数据库连接地址
+    MONGO: 'mongodb://127.0.0.1/photo',
+    // 访问端口
+    PORT: 3000,
+};
+
---
+++ server/server.js
@@ -0,0 +1,26 @@
+'use static';
+
+var app = require('koa')();
+var serve = require('koa-static');
+var router = require('koa-router')();
+var bodyParser = require('koa-body-parser');
+
+var requestAPI = require("./restapi/RequestAPI");
+
+var env = require('./env');
+
+// 将request中的数据放在this.request.body中
+app.use(bodyParser());
+// 引用所有静态文件
+app.use(serve(__dirname + '/../dist'));
+
+// 引用所有请求
+app.use(requestAPI.routes());
+
+app.use(router.allowedMethods());
+
+
+
+// 设置端口
+app.listen(env.PORT);
+console.log('http://localhost:' + env.PORT+'/');
