555676c859393225fbd5668cf02d94bda2a1bc29
zhou fei <fzhou@cccis.com>
2017-02-27 10:05:53 +0800

    db配置修改

--- server/env.js
+++ server/env.js
@@ -1,7 +1,7 @@
 module.exports = {
     // mongo数据库连接地址
-    // MONGO: 'mongodb://127.0.0.1/photos',
-    MONGO: 'mongodb://192.168.200.9/photos',
+    MONGO: 'mongodb://127.0.0.1/photos',
+    // MONGO: 'mongodb://192.168.200.9/photos',
     // 访问端口
     PORT: 3000,
 };
